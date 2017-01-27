b3e3abb3e3acb9347ceecf22d4de74ff8788fe6f
tetsu100 <tetsu100@mail.chapman.edu>
2017-01-27 15:06:51 -0800

    Added stats

--- visuals.py
+++ visuals.py
@@ -4,69 +4,46 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mping
 import numpy as np
 
-def visualizeRadius(data, location, window):
+def visualizeRadius(data, location):
     plt.axis('equal')
-    plt.clf()
-    window.statusUpdate("Getting all elevations...")
-    elevationSet = [d["ELEVATION"] for d in data if "ELEVATION" in d]
-    elevationSet = [d for d in elevationSet if type(d) != str]
+    elevationSet = [d["ELEVATION"] for d in data]
+    elevationSet = [y for y in elevationSet if not isinstance(y, str)]
     ##Get the range of values covered.
     minval = min(elevationSet)
     maxval = max(elevationSet)
     rangeval = maxval-minval
-    count = 0
-    total = len(data)
-    oldPercent = 0
+
     for d in data:
-        if "ELEVATION" in d:
-            if type(d["ELEVATION"]) != str:
-                y = d["PARCEL LEVEL LATITUDE"]-location[0]
-                x = d["PARCEL LEVEL LONGITUDE"]-location[1]
-                p = (d["ELEVATION"]-minval)/rangeval #Percent of where it stands in the elevation graph.
-                rgb = (0, 0, 0)
-                if y != 'NULL' and x != 'NULL':
-                    if (d.get("isGeo", 0)): #if the data point isn't a house, assign it to a different color scheme
-                        rgb = (p, 1-p, 0)
-                        plt.plot([x],[y],"^",color=rgb)
-                    else:
-                        rgb = (p, 0, 1-p)
-                        plt.plot([x],[y],"o",color=rgb)
-        count += 1
-        percent = round((count/total)*100, 1)
-        if oldPercent != percent:
-            oldPercent = percent
-            window.statusUpdate("Plotting points: "+str(oldPercent)+"%")
+        rgb = (0,0,0)
+        y = d["PARCEL LEVEL LATITUDE"]-location[0]
+        x = d["PARCEL LEVEL LONGITUDE"]-location[1]
+        try:
+            p = (d["ELEVATION"]-minval)/rangeval #Percent of where it stands in the elevation graph.
+            if (d.get("isGeo", 0)): #if the data point isn't a house, assign it to a different color scheme
+                rgb = (p, 1-p, 0)
+            else:
+                rgb = (p, 0, 1-p)
+            plt.plot([x],[y],"o",color=rgb)
+        except:
+            continue
     plt.plot(0,0,"o",color="green")
 
-def visualizeSet(data, window):
+def visualizeSet(data):
     plt.axis('equal')
-    plt.clf()
-    window.statusUpdate("Getting all elevations...")
-    elevationSet = [d["ELEVATION"] for d in data if "ELEVATION" in d]
-    elevationSet = [d for d in elevationSet if type(d) != str]
+    elevationSet = [d["ELEVATION"] for d in data]
     ##Get the range of values covered.
     minval = min(elevationSet)
     maxval = max(elevationSet)
     rangeval = maxval-minval
-    count = 0
-    total = len(data)
-    oldPercent = 0
+
     for d in data:
-        if "ELEVATION" in d:
-            if type(d["ELEVATION"]) != str:
-                y = d["PARCEL LEVEL LATITUDE"]
-                x = d["PARCEL LEVEL LONGITUDE"]
-                p = (d["ELEVATION"]-minval)/rangeval #Percent of where it stands in the elevation graph.
-                rgb = (p, 0, 1-p)
-                if y!='NULL' and x != 'NULL':
-                    plt.plot([x],[y],d["ELEVATION"],marker="o",color=rgb)
+        y = d["PARCEL LEVEL LATITUDE"]
+        x = d["PARCEL LEVEL LONGITUDE"]
+        p = (d["ELEVATION"]-minval)/rangeval #Percent of where it stands in the elevation graph.
+        rgb = (p, 0, 1-p)
+        plt.plot([x],[y],"o",color=rgb)
+    plt.plot(0,0,"o",color="green")
 
-        count += 1
-        percent = round((count/total)*100, 1)
-        if oldPercent != percent:
-            oldPercent = percent
-            window.statusUpdate("Plotting points: "+str(oldPercent)+"%")
-
-def display(name):
+def display(name, stats):
     plt.title(name)
     plt.show(block=False)
